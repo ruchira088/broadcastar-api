422e37f5232fa2da4d2dec89371332f3f9861df6
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-05-21 20:57:02 +1000

    Renaming keys in the ConfigMap

--- app/info/ApplicationInformation.scala
+++ app/info/ApplicationInformation.scala
@@ -1,5 +1,10 @@
 package info
 
-class ApplicationInformation {
+import org.joda.time.DateTime
+import play.api.libs.json.{Json, OWrites}
 
+case class ApplicationInformation(gitBranch: Option[String], gitCommit: Option[String], buildTimestamp: Option[DateTime])
+
+object ApplicationInformation {
+  implicit val applicationInformationWrites: OWrites[ApplicationInformation] = Json.writes[ApplicationInformation]
 }
