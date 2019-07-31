1bcfd425b9329430d0f61f1f583f43e3b2d0912c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-07-31 13:51:32 +1000

    Updating the Initialization application

--- initialization/src/main/scala/com/ruchij/init/InitializationApp.scala
+++ initialization/src/main/scala/com/ruchij/init/InitializationApp.scala
@@ -4,9 +4,9 @@ import akka.actor.ActorSystem
 import com.ruchij.shared.ec.IOExecutionContextImpl
 import com.ruchij.shared.kafka.KafkaTopic
 import com.ruchij.shared.kafka.admin.{KafkaAdministrator, KafkaAdministratorImpl}
-import com.ruchij.shared.kafka.config.KafkaClientConfiguration
+import com.ruchij.shared.kafka.config.{KafkaClientConfiguration, KafkaTopicConfiguration}
 import com.ruchij.shared.monads.MonadicUtils
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.Logger
 
 import scala.concurrent.Future
@@ -17,18 +17,28 @@ object InitializationApp {
   private val logger = Logger[InitializationApp.type]
 
   def main(args: Array[String]): Unit = {
+    val config: Config = ConfigFactory.load()
     implicit val actorSystem: ActorSystem = ActorSystem("initialization-app")
     implicit val ioExecutionContext: IOExecutionContextImpl = new IOExecutionContextImpl(actorSystem)
 
-    val kafkaConfiguration = MonadicUtils.unsafe(KafkaClientConfiguration.parse(ConfigFactory.load()))
-    val kafkaAdministrator: KafkaAdministrator = new KafkaAdministratorImpl(kafkaConfiguration, ioExecutionContext)
+    val (confluentKafkaClientConfiguration, kafkaTopicConfiguration) =
+      MonadicUtils.unsafe {
+        for {
+          confluentKafkaClientConfiguration <- KafkaClientConfiguration.parseConfluentConfig(config)
+          kafkaTopicConfiguration <- KafkaTopicConfiguration.parse(config)
+        }
+        yield (confluentKafkaClientConfiguration, kafkaTopicConfiguration)
+
+      }
+
+    val kafkaAdministrator: KafkaAdministrator = new KafkaAdministratorImpl(confluentKafkaClientConfiguration, kafkaTopicConfiguration, ioExecutionContext)
 
     val result =
       Future.sequence {
         KafkaTopic.topics.map { kafkaTopic =>
-          kafkaAdministrator.createTopic(kafkaTopic.name(kafkaConfiguration))
+          kafkaAdministrator.createTopic(kafkaTopic.name(kafkaTopicConfiguration.topicPrefix))
             .map {
-              kafkaTopic.name(kafkaConfiguration) -> _
+              kafkaTopic.name(kafkaTopicConfiguration.topicPrefix) -> _
             }
         }
       }
